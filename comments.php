6e5bd55e3ae5463039b48c899b9c39aa59f00ec4
Aivanova2003 <a.ivvanovaa03@mail.ru>
2025-05-26 21:17:08 +0300

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * The template for displaying comments.
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Astra
- * @since 1.0.0
- */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
