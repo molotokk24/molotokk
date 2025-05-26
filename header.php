e96d88985963c1b06e9eeb500e585c04b4376b90
Aivanova2003 <a.ivvanovaa03@mail.ru>
2025-05-26 21:16:29 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * The header for Astra Theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Astra
- * @since 1.0.0
- */
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
